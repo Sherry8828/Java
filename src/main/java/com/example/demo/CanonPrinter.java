8ee0709851e8b0bd22bb1a3def7a5482d814e90a
Sherry <nt45139@gmail.com>
2024-09-11 19:41:36 +0800

    兩種Bean初始化的方式

--- src/main/java/com/example/demo/CanonPrinter.java
+++ src/main/java/com/example/demo/CanonPrinter.java
@@ -1,12 +1,18 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
 //@Component
-public class CanonPrinter implements Printer {
-
+public class CanonPrinter implements Printer, InitializingBean {
+    private int count;
     @Override
     public void print(String message) {
         System.out.println("Canon印表機:"+message);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        //初始化
     }
 }
--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
     @Autowired
-    @Qualifier("yourPrinter")
+    @Qualifier("hpPrinter")
     private Printer printer;
 
 
