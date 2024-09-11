b66c4dc11e88304e6d22e1a083deff6bcf0ed95a
Sherry <nt45139@gmail.com>
2024-09-11 17:06:59 +0800

    first commit CH2

---
+++ src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    @Autowired
+    //@Qualifier("canonPrinter")
+    private Printer printer;
+
+
+    @RequestMapping("/test")
+    public String test(){
+        System.out.println("Hi!");
+        printer.print("Hello 你好嗎");
+        return "Hello World!";
+    }
+}
