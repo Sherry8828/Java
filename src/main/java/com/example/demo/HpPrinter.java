69a4caecb8d2041d4d46e01cbdf7be5393510caf
Sherry <nt45139@gmail.com>
2024-09-16 13:27:37 +0800

    sprint boot設定檔應用

--- src/main/java/com/example/demo/HpPrinter.java
+++ src/main/java/com/example/demo/HpPrinter.java
@@ -1,21 +1,23 @@
 package com.example.demo;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 @Component
 public class HpPrinter implements Printer {
 
+    @Value("${printer.name}")
+    private String name;
+
+    @Value("${printer.count}")
     private int count;
 
-    @PostConstruct
-    public void init(){
-        count = 5;
-    }
 
     @Override
     public void print(String message) {
         count--;
         System.out.println("HP印表機:"+message);
+        System.out.println(name+":"+message);
         System.out.println("剩餘使用次數"+count);
     }
 }
