728ae15daead220dc9c38bef7e492ea3f12e1e30
Sherry <nt45139@gmail.com>
2024-09-21 19:28:08 +0800

    ch4-5:回傳 Json 格式

---
+++ src/main/java/com/example/demo/MyController2.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class MyController2 {
+    @RequestMapping("/product")
+    public Store product(){
+        Store store = new Store();
+        List<String> list=new ArrayList<>();
+        list.add("蘋果");
+        list.add("橘子");
+        store.setProductList(list);
+        return store;
+    }
+
+    @RequestMapping("/user")
+    public Student user(){
+        Student student = new Student();
+        student.setName("Judy");
+        return student;
+    }
+
+}
---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,79 @@
+package com.example.demo;
+
+import java.util.List;
+
+public class Student {
+    Integer id;
+    String name;
+    Double score;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public boolean isGraduated() {
+        return graduated;
+    }
+
+    public void setGraduated(boolean graduated) {
+        this.graduated = graduated;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public List<String> getCourseList() {
+        return courseList;
+    }
+
+    public void setCourseList(List<String> courseList) {
+        this.courseList = courseList;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+
+    public List<Pet> getPetList() {
+        return petList;
+    }
+
+    public void setPetList(List<Pet> petList) {
+        this.petList = petList;
+    }
+
+    boolean graduated;
+    Integer age;
+    List<String> courseList;
+    Pet pet;
+    List<Pet> petList;
+}
